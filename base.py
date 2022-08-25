0d5c20e151df47e74fb7f34ba34fe4a8c928e9ce
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2022-08-25 22:29:33 +0300

    Version='1.3'. Simplify code base and design

--- base.py
+++ base.py
@@ -1,11 +1,17 @@
 import logging, json
 import logging.config
-import time
-from os import SEEK_END
 from os import path
 logging.config.fileConfig(path.join(path.dirname(path.abspath(__file__)), 'logging.ini'))
-from exceptions import LogViewerException
 
+class LogViewerException(Exception):
+    """Base applicaton Exception"""
+    def __init__(self, message="Error occured"):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        logging.error(f"{self.message}", exc_info=1)
+        return f'-> {self.message}'
 class Configuration(object):
     """
     JSON configuration object\n
---
+++ main.py
@@ -0,0 +1,35 @@
+from waitress import serve
+from base import Configuration, LogViewerException, logging
+from flask import Flask, render_template
+from time import sleep
+
+c = Configuration(confFile="configuration_dev.json")
+server = Flask(__name__)
+logger = logging.getLogger(server.name)
+
+@server.route(c.viewEndpoint)
+def gui():
+    logger.info("Request to GUI")
+    return render_template('index.html')
+
+@server.route(c.streamEndpoint)
+def stream():
+    logger.info("XMLHttpRequest for data stream")
+    def generate():
+        with open(c.logPath) as f:
+            while True:
+                yield f.read()
+                sleep(1)
+
+    return server.response_class(generate(), mimetype='text/plain')
+
+if __name__ == "__main__":
+    try:
+        logging.info(f"---------- Application start ----------")        
+        logging.info(f"Streming endpoint: http://{c.host}:{c.port}{c.streamEndpoint}")
+        logging.info(f"GUI endpoint: http://{c.host}:{c.port}{c.viewEndpoint}")
+        serve(server, host=c.host, port=c.port)
+    except Exception as e:
+        raise LogViewerException(f"Error occured in {__name__} module. Exception: {e}")
+    finally:
+        logging.info(f"---------- Application end ----------")
